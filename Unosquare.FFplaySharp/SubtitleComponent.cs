96b3f5a18b2b97eb0bbae735dc3a2046d6a749bb
Mario Di Vece <mario@unosquare.com>
2020-12-21 19:46:28 -0600

    before mergining component and decoder

---
+++ Unosquare.FFplaySharp/SubtitleComponent.cs
@@ -0,0 +1,21 @@
+﻿namespace Unosquare.FFplaySharp
+{
+    using FFmpeg.AutoGen;
+
+    public unsafe sealed class SubtitleComponent : MediaComponent
+    {
+        public SubtitleComponent(MediaContainer container)
+            : base(container)
+        {
+            // placeholder
+        }
+
+        public SwsContext* ConvertContext;
+
+        public new SubtitleDecoder Decoder { get; set; }
+
+        public override AVMediaType MediaType => AVMediaType.AVMEDIA_TYPE_SUBTITLE;
+
+        protected override FrameQueue CreateFrameQueue() => new(Packets, Constants.SUBPICTURE_QUEUE_SIZE, false);
+    }
+}
---
+++ Unosquare.FFplaySharp/VideoComponent.cs
@@ -0,0 +1,22 @@
+﻿namespace Unosquare.FFplaySharp
+{
+    using FFmpeg.AutoGen;
+
+    public unsafe sealed class VideoComponent : FilteringMediaComponent
+    {
+        public VideoComponent(MediaContainer container)
+            : base(container)
+        {
+            // placeholder
+        }
+
+        public SwsContext* ConvertContext;
+
+        public new VideoDecoder Decoder { get; set; }
+
+        public override AVMediaType MediaType => AVMediaType.AVMEDIA_TYPE_VIDEO;
+
+        protected override FrameQueue CreateFrameQueue() => new(Packets, Constants.VIDEO_PICTURE_QUEUE_SIZE, true);
+    }
+
+}
